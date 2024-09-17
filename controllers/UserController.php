ab445dd3d4bcca42461fff9183278f4aedd5e172
amandvitoria <amandvitoria244@gmail.com>
2024-09-17 12:50:43 -0300

    metodos para puxar informações de outros arquivos

---
+++ controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+// Definição da classe UserController, responsavel por gerenciar as ações relacionadas aos usuarios
+class UserController
+{
+    public function register(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $data = [
+                'nome' => $_POST['nome'],
+                'email' => $_POST['email'],
+                'senha' => password_hash($_POST['senha'], PASSWORD_DEFAULT),
+                'perfil' => $_POST['perfil']
+
+            ];
+            User::create($data);
+
+        }else{ 
+            // sse a requisição não for POST (por exemplo, GET), carrega a pagina de registro
+            include ' views/register.php';
+        }
+    }
+}
+
+
+
+
+?>
--- controllers/authController.php
+++ controllers/authController.php
@@ -8,7 +8,10 @@ class  AuthController
     // Cria função responsável pelo processo de login
     public function login()
     {
-       
+        // Verifica se a requisição HTTP é tipo POST, ou seja, se o formulario foi enviado
+       if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+       }
     }
 }
 ?>
