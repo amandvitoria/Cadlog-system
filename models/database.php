105231fe050c214c846792fd66445116ea869c6f
amandvitoria <amandvitoria244@gmail.com>
2024-09-17 12:51:10 -0300

    criando estrutura

---
+++ models/database.php
@@ -0,0 +1,8 @@
+<?php
+class Database{
+    // utiliza padrão singleton, cujo objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execuç~so do programa , e que essa instancia seja utilizada sempre que nescessario
+}
+
+
+
+?>
