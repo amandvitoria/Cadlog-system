ad9b8b83c9be24631040108d4f97a6218adc16ec
amandvitoria <amandvitoria244@gmail.com>
2024-09-18 10:08:21 -0300

    criando padrão singleton para garantir uma unica instancia, um retorno a conexão com bd, verificação da intancia de conexão, conexão drive mysql, define o modo de erro para exceções

--- models/database.php
+++ models/database.php
@@ -1,6 +1,28 @@
 <?php
 class Database{
     // utiliza padrão singleton, cujo objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execuç~so do programa , e que essa instancia seja utilizada sempre que nescessario
+    private static $instanc = null;
+
+     //Método público que retorna a conexão com o BD
+    public static function getconnection(){
+        //verifica se a instancia de conexão ainda não foi criada
+        if(!self::$instanc){
+            $host       ='localhost';
+            $db         = 'sistema_usuarios';
+            $user       = 'root';
+            $password   = '';
+
+            // A conexão usa o driver Mysql (Mysql:) e as informações de host e db
+            self::$instanc = new PDO("Mysql:host=$host;dbname=$db",$user, $password);
+
+            //Define o modo de erro para exceções, facilitando a depuração e tratamento do erro
+            self::$instanc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instanc;
+
+    }
+
+
 }
 
 
