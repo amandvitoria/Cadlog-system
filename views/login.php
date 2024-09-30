abdc82ef7372b5fdc34759418db487a5f0bdf744
amandvitoria <amandvitoria244@gmail.com>
2024-09-30 08:02:10 -0300

    estilizando paginas

--- views/login.php
+++ views/login.php
@@ -6,21 +6,85 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrar</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            text-align: center;
+            background: rgb(68, 81, 196);
+            background: rgb(219, 244, 239);
+            background: radial-gradient(circle, rgba(219, 244, 239, 1) 0%, rgba(70, 106, 182, 1) 100%);
+            margin-top: 150px;
+            margin-left: 400px;
+            margin-right: 400px;
+            
+
+        }
+
+        div {
+            border: 5px;
+            width: 60%;
+            background-color: white;
+            border-radius: 5px;
+            left: 100px;
+            position: relative;
+
+        }
+
+        input{
+            padding: 7px;
+            border-radius: 5px;
+            border: 1px solid black;
+        }
+
+        input:focus{
+            border-color: blue;
+            outline: none;
+            box-shadow: 0px 2px 3px blue;
+        }
+
+        button {
+            padding: 10px;
+            background-color: lightblue;
+            border: 1px;
+            border-radius: 10px;
+            color: black;
+        }
+
+        button:hover {
+            background-color: blue;
+            color: white;
+            transition: color 0.3s;
+        }
+
+        label{
+            font-size: large;
+        }
+
+        a{
+            color: black;
+        }
+        a:hover{
+            color: blue;
+        }
+        
+    </style>
 </head>
 
 <body>
     <!-- O formulário usa o método POST para enviar dados de forma segura -->
     <!-- Os dados serão enviados para 'index.php' com a ação 'login' -->
     <main>
-    <div>
-        <form method="post" action="index.php?action=login">
-            <h2>Tela de Login</h2>
-            <input type="email" name="email" placeholder="Email" required><br>
-            <input type="password" name="senha" placeholder="Senha" required><br><br>
-            <button type="submit">Login</button>
-        </form>
-        <!-- Define o destino do link e leva à opção de cadastro -->
-        <a href="index.php?action=register">Cadastrar-se</a>
+        
+        <div><br>
+            <form method="post" action="index.php?action=login">
+                <h2>Tela de Login</h2>
+                <label for="nome">Nome:</label><br>
+                <input type="email" name="email" placeholder="Email" required><br><br>
+                <label for="nome">Senha:</label><br>
+                <input type="password" name="senha" placeholder="Senha" required><br><br>
+                <button type="submit">Login</button><br><br>
+            </form>
+            <!-- Define o destino do link e leva à opção de cadastro -->
+            <a href="index.php?action=register">Cadastrar-se</a>
         </div>
     </main>
 
--- views/register.php
+++ views/register.php
@@ -1,34 +1,108 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar-se</title>
+    <style>
+        body {
+            text-align: center;
+            background: rgb(68, 81, 196);
+            background: rgb(219, 244, 239);
+            background: radial-gradient(circle, rgba(219, 244, 239, 1) 0%, rgba(70, 106, 182, 1) 100%);
+            margin-top: 100px;
+            margin-left: 400px;
+            margin-right: 400px;
+            display: flex;
+            height:  450px;
+
+        }
+
+        div {
+            border: 5px;
+            width: 60%;
+            background-color: white;
+            border-radius: 5px;
+            left: 100px;
+            position: relative;
+
+        }
+
+        input {
+            padding: 7px;
+            border-radius: 5px;
+            border: 1px solid black;
+        }
+
+        input:focus {
+            border-color: blue;
+            outline: none;
+            box-shadow: 0px 2px 3px blue;
+        }
+
+        button {
+            padding: 7px;
+            background-color: lightblue;
+            border: 1px;
+            border-radius: 10px;
+            color: black;
+        }
+
+        button:hover {
+            background-color: blue;
+            color: white;
+            transition: color 0.7s;
+        }
+
+        select {
+            padding: 7px;
+            border-radius: 5px;
+            border: 1px solid;
+        }
+
+        select:hover {
+            border: color blue;
+            outline: none;
+        }
+        label{
+            font-size: large;
+        }
+
+        a{
+            color: black;
+        }
+        a:hover{
+            color: blue;
+        }
+    </style>
 </head>
+
 <body>
     <div>
         <h2>Cadastro de usuário</h2>
         <form action="" method="post">
-            <label for="nome"></label>
-            <input type="text" name="nome" id="nome "require>
+            <label for="nome">Nome:</label><br>
+            <input type="text" name="nome" id="nome " require><br><br>
 
-            <label for="email">Email</label>
-            <input type="emeil" name="email" id="email">
+            <label for="email">Email:</label><br>
+            <input type="emeil" name="email" id="email" required><br><br>
 
-            <label for="senha">Senha</label>
-            <input type="passwor" name="senha" id="senha" required>
+            <label for="senha">Senha:</label><br>
+            <input type="passwor" name="senha" id="senha" required><br><br>
 
-            <label for="perfil">Perfil:</label>
+            <label for="perfil">Perfil:</label><br>
             <select name="perfil" id="perfil">
                 <option value="admim">Admim</option>
                 <option value="gestor">Gestor</option>
                 <option value="colaborador">Colaborador</option>
-                
-            </select>
-            <button type="submit">Cadastrar</button>
+
+            </select><br><br>
+            <button type="submit">Cadastrar</button><br><br>
         </form>
-        
+
         <a href="">Voltar ao Login</a>
     </div>
 </body>
+
 </html>
